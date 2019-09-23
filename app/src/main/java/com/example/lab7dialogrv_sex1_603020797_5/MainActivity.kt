6ec84de48835726cadde061c398b88a7dfb6bd87
phumrapee53598 <54529472+phumrapee53598@users.noreply.github.com>
2019-09-23 17:13:50 +0700

    Lab7DialogRV_Sec1_603020797-5

---
+++ app/src/main/java/com/example/lab7dialogrv_sex1_603020797_5/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.lab7dialogrv_sex1_603020797_5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.add_dialog_layut.view.*
+
+class MainActivity : AppCompatActivity() {
+    val studentList = arrayListOf<Student>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        testStudentData()
+        recycler_view.adapter = StudentsAdapter(this.studentList,applicationContext)
+        recycler_view.layoutManager = LinearLayoutManager(applicationContext)
+        recycler_view.itemAnimator = DefaultItemAnimator()
+    }
+    fun addStudent(v:View){
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.add_dialog_layut,null)
+
+        val mBuilder = AlertDialog.Builder(this)
+        mBuilder.setView(mDialogView)
+        val mAlertDialog = mBuilder.show()
+
+        mDialogView.btnAdd.setOnClickListener {
+        studentList.add(Student(mDialogView.edt_id.text.toString(),mDialogView.edt_name.text.toString(),mDialogView.edit_age.text.toString().toInt()))
+           recycler_view.adapter?.notifyDataSetChanged()
+            Toast.makeText(applicationContext,"The student is added successfully",Toast.LENGTH_SHORT).show()
+            mAlertDialog.dismiss()
+        }
+        mDialogView.btnCancel.setOnClickListener(){
+            mAlertDialog.dismiss()
+        }
+    }
+    fun testStudentData(){
+        studentList.add(Student("603020797-5","Student 1",20))
+        studentList.add(Student("603020797-6","Student" +
+                " 2",21))
+    }
+}
